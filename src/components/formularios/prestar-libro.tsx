39b562244cbbc92d491bc5af3bae5d4f944bab4c
León <leonginel@gmail.com>
2025-08-25 16:21:56 +0200

    Funcionalidad de prestar-libro

--- src/components/formularios/prestar-libro.tsx
+++ src/components/formularios/prestar-libro.tsx
@@ -1,22 +1,25 @@
 import type { Libro } from "../../types/libro-interface";
 import styles from "./prestar-libro.module.css";
 import BuscadorLibros from "../buscador/buscador-libros";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { LibrosContext } from "../../contexts/libros-context";
 
 export default function PrestarLibro() {
-  const { libros } = useContext(LibrosContext);
+  const { libros, setLibros } = useContext(LibrosContext);
 
-  const [listaLibros, setListaLibros] = useState(libros);
+  const libroPrestado = (libro: Libro) => {
+    const confirmar = window.confirm(`¿Deseas prestar "${libro.titulo}"?`);
+    if (!confirmar) return;
 
-  const prestarLIbro = (libro: Libro) => {
-    setListaLibros((prev) => prev.map((l) => (l.titulo === libro.titulo ? { ...l, disponible: false } : l)));
+    setLibros((prev) => prev.map((l) => (l.titulo === libro.titulo ? { ...l, disponible: false } : l)));
+
+    alert(`El libro "${libro.titulo}" ha sido prestado`);
   };
 
   return (
     <div className={styles["prestar-libro"]}>
       <h2>Prestar un libro:</h2>
-      <BuscadorLibros libros={listaLibros} onSelect={prestarLIbro} />
+      <BuscadorLibros libros={libros} onSelect={libroPrestado} />
     </div>
   );
 }
